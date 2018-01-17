477d49ed54b97589f5eb44e76af087587467e25b
Harel Assayag <harel@scylladb.com>
2018-01-17 15:27:05 +0200

    small fixes to corrupt and rebuild

--- corrupt_then_rebuild_test.py
+++ corrupt_then_rebuild_test.py
@@ -20,16 +20,19 @@ from sdcm.nemesis import CorruptThenRebuildMonkey
 
 class CorruptThenRebuildTest(ClusterTester):
     """
-    Cost rest space to trigger ENOSPC error for scylla,
-    and then release space to recover scylla service.
+
     :avocado: enable
     """
     def test_corrupt_then_rebuild_nodes(self):
-        self.db_cluster.add_nemesis(nemesis=CorruptThenRebuildMonkey,
-                                    loaders=self.loaders,
-                                    monitoring_set=self.monitors)
-
         for i in range(int(self.params.get('iterations'))):
+            # run a prepare write workload
+            stress_queue = self.run_stress_thread(stress_cmd=self.params.get('prepare_stress_cmd'),
+                                                  stress_num=1,
+                                                  keyspace_num=1)
+            self.get_stress_results(queue=stress_queue)
+            self.db_cluster.add_nemesis(nemesis=CorruptThenRebuildMonkey,
+                                        loaders=self.loaders,
+                                        monitoring_set=self.monitors)
             self.db_cluster.start_nemesis(interval=15)
             self.db_cluster.stop_nemesis(timeout=1000)
             stress_queue = self.run_stress_thread(stress_cmd=self.params.get('stress_read_cmd'),
